32c38786d3252329648554a5a04365e7b30f5e29
René Juan Rico Mendoza <rricomendoza@cideso.mil.ar>
2016-09-07 14:06:44 -0300

    Prueba con OpenTK

---
+++ TGC.OpenTK/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TGC.OpenTK
+{
+	public class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			// The 'using' idiom guarantees proper resource cleanup.
+			// We request 30 UpdateFrame events per second, and unlimited
+			// RenderFrame events (as fast as the computer can handle).
+			using (Game game = new Game())
+			{
+				game.Run(30.0);
+			}
+		}
+	}
+}
+
